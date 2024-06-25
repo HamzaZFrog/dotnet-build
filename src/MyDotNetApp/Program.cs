3d6f27621b2225892af274bef0726821ea1d2451
Hamza Zaoui <Hamzaz@jfrog.com>
2024-06-25 19:13:14 +0200

    Moe dependencies

--- src/MyDotNetApp/Program.cs
+++ src/MyDotNetApp/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using Newtonsoft.Json;
 using System.Text.Encodings.Web;
+using Halibut;
+using Halibut.Transport;
+using Halibut.Transport.Protocol;
+using Wire;
+using System.Security.Cryptography.Xml;
+using QuantConnect;
 
 namespace MyDotNetApp
 {
@@ -16,7 +22,27 @@ namespace MyDotNetApp
             var encoder = HtmlEncoder.Default;
             string htmlEncodedString = encoder.Encode("<script>alert('Hello World');</script>");
             Console.WriteLine($"HTML Encoded String: {htmlEncodedString}");
+
+            // Using Halibut
+            var endpoint = new ServiceEndPoint(new Uri("https://example.com"), "thumbprint");
+            Console.WriteLine($"Halibut Endpoint URI: {endpoint.BaseUri}");
+
+            // Using Wire
+            var serializer = new Serializer();
+            var serializedPerson = serializer.Serialize(person);
+            Console.WriteLine($"Serialized Person using Wire: {Convert.ToBase64String(serializedPerson)}");
+
+            // Using System.Security.Cryptography.Xml
+            var xmlDocument = new System.Xml.XmlDocument();
+            xmlDocument.LoadXml("<root><test>Example</test></root>");
+            var signedXml = new SignedXml(xmlDocument);
+            Console.WriteLine($"XML Document: {xmlDocument.OuterXml}");
+
+            // Using QuantConnect.Common
+            var qcUser = new QuantConnect.Api.Api();
+            Console.WriteLine("QuantConnect API User Created");
         }
     }
 }
+
 
